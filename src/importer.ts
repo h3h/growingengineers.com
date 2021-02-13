c6a4bacc7ba7d47bd3a98043f90188d094916d9e
Brad Fults <bfults@gmail.com>
2021-02-13 13:50:32 -0600

    Get Importer class validating a basic config

--- src/importer.ts
+++ src/importer.ts
@@ -1,4 +1,4 @@
-import * as YAML from "yaml"
+import * as YAML from "js-yaml"
 
 type ConfigRoles = Array<string>
 type ConfigLevels = Map<string, Map<number, string>>
@@ -12,24 +12,58 @@ interface ConfigAttributes {
     [dimension: string]: Array<ConfigAttributeDetail>
   }
 }
-interface ValidConfig {
+export interface ValidConfig {
+  v: number
   roles: ConfigRoles
   levels: ConfigLevels
   attributes: ConfigAttributes
 }
 
 export class Importer {
-  config: Map<string, string>
+  config: ValidConfig
   data: string
-  onerror: (errors: Array<string>) => void
+  onerror: (errors: Error[]) => void
 
   constructor(data: string) {
     this.data = data
   }
 
   parse(): boolean {
-    let parsedConfig: ValidConfig = YAML.parse(this.data)
-    console.log(parsedConfig)
-    return true
+    try {
+      let parsed = YAML.load(this.data) as ValidConfig
+      let errors = this.validateDocument(parsed)
+      if (0 === errors.length) {
+        this.config = parsed
+        return true
+      } else {
+        typeof this?.onerror == "function" && this.onerror(errors)
+      }
+    } catch (e) {
+      typeof this?.onerror == "function" && this.onerror([e])
+    }
+    return false
+  }
+
+  validateDocument(doc: ValidConfig): Error[] {
+    let errors: Error[] = []
+
+    if (!("number" === typeof doc.v)) {
+      errors.push(new Error("Missing property 'v', a numerical version number"))
+    }
+
+    if (!(Array.isArray(doc.roles) && "string" === typeof doc.roles[0])) {
+      errors.push(new Error("Missing property 'roles', an array of strings"))
+    }
+
+    if (!(doc.hasOwnProperty("levels") && doc.levels)) {
+      errors.push(
+        new Error(
+          "Missing property 'levels', a map of string _roles_, each to a map " +
+            "of number _level_ to string _title_"
+        )
+      )
+    }
+
+    return errors
   }
 }
--- test/importer.spec.ts
+++ test/importer.spec.ts
@@ -1,13 +1,37 @@
-import assert = require("assert")
 import { describe } from "mocha"
+import { expect } from "chai"
 import { Importer } from "../src/importer"
-import * as fs from "fs"
+import { readFileSync } from "fs"
+import * as sinon from "sinon"
 
 describe("Importer", function () {
-  it("gives a result", function () {
-    let importer = new Importer(
-      fs.readFileSync(__dirname + "/../data/import.yaml").toString()
-    )
-    assert.strict(importer.parse())
+  var invalidData: string = "banana town"
+  var validData: string
+  var importer: Importer
+
+  describe("with a valid config", function () {
+    before(function () {
+      validData = readFileSync(__dirname + "/../data/import.yaml").toString()
+    })
+
+    it("#parse returns true", function () {
+      importer = new Importer(validData)
+      expect(importer.parse()).to.be.true
+    })
+  })
+
+  describe("with an invalid config", function () {
+    it("#parse returns false", function () {
+      importer = new Importer(invalidData)
+      expect(importer.parse()).to.be.false
+    })
+
+    it("calls its onerror function and passes an array of Errors", function () {
+      importer = new Importer(invalidData)
+      importer.onerror = sinon.spy()
+      importer.parse()
+      // @ts-ignore: spy methods confuse the param types
+      expect(importer.onerror.getCall(0).args[0][0]).to.be.an("Error")
+    })
   })
 })
